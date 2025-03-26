0e0b03fabc455dd744d36114e41489115d8a4e29
p2r3 <41925384+p2r3@users.noreply.github.com>
2025-03-26 19:50:41 +0200

    feat: add lights and func_wall

--- main.js
+++ main.js
@@ -91,16 +91,33 @@ let output = "";
 class Vector {
 
   constructor (x = 0, y = 0, z = 0) {
-    if (isNaN(x)) throw "x component is not a number";
+    if (isNaN(x)) throw "X component is not a number";
     this.x = Number(x);
-    if (isNaN(y)) throw "y component is not a number";
+    if (isNaN(y)) throw "Y component is not a number";
     this.y = Number(y);
-    if (isNaN(z)) throw "z component is not a number";
+    if (isNaN(z)) throw "Z component is not a number";
     this.z = Number(z);
   }
 
   static fromString (str) {
-    return new Vector(...str.split(" ").map(Number));
+    return new Vector(...str.trim().replaceAll("  ", " ").split(" ").map(Number));
+  }
+  toString () {
+    return `${this.x} ${this.y} ${this.z}`;
+  }
+
+  static fromAngles (ang) {
+    // Convert degrees to radians
+    ang = ang.copy().scale(Math.PI / 180.0);
+    // Compute sines and cosines of angles
+    const cy = Math.cos(ang.y), sy = Math.sin(ang.y);
+    const cp = Math.cos(ang.x), sp = Math.sin(ang.x);
+    // Calculate and return the forward vector
+    return new Vector(cy * cp, sy * cp, -sp);
+  }
+
+  copy () {
+    return new Vector(this.x, this.y, this.z);
   }
 
   scale (factor) {
@@ -109,16 +126,173 @@ class Vector {
     this.z *= factor;
     return this;
   }
-
   add (other) {
     this.x += other.x;
     this.y += other.y;
     this.z += other.z;
     return this;
   }
+  sub (other) {
+    this.x -= other.x;
+    this.y -= other.y;
+    this.z -= other.z;
+    return this;
+  }
+  cross (other) {
+    return new Vector(
+      this.y * other.z - this.z * other.y,
+      this.z * other.x - this.x * other.z,
+      this.x * other.y - this.y * other.x
+    );
+  }
+  normalize () {
+    const length = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    if (length === 0) throw "Cannot normalize a zero-length vector";
+    return this.scale(1.0 / length);
+  }
 
+  /**
+   * Returns true if both vectors are nearby within a tiny epsilon.
+   * Used to combat floating point errors in axis checks.
+   */
+  near (other) {
+    return (
+      Math.abs(this.x - other.x) < 0.000001 &&
+      Math.abs(this.y - other.y) < 0.000001 &&
+      Math.abs(this.z - other.z) < 0.000001
+    );
+  }
+
+  // Axis vectors for comparison in `getAxis`
+  static xVector = new Vector(1, 0, 0);
+  static _xVector = new Vector(-1, 0, 0);
+  static yVector = new Vector(0, 1, 0);
+  static _yVector = new Vector(0, -1, 0);
+  static zVector = new Vector(0, 0, 1);
+  static _zVector = new Vector(0, 0, -1);
+
+  /**
+   * Returns an index representing the axis that this vector is pointing
+   * to, or `null` if the vector isn't axis-aligned. Possible values:
+   * 0: X (East)
+   * 1: -X (West)
+   * 2: Y (North)
+   * 3: -Y (South)
+   * 4: Z (Up)
+   * 5: -Z (Down)
+   */
+  getAxis () {
+    if (this.near(Vector.xVector)) return 0;
+    if (this.near(Vector._xVector)) return 1;
+    if (this.near(Vector.yVector)) return 2;
+    if (this.near(Vector._yVector)) return 3;
+    if (this.near(Vector.zVector)) return 4;
+    if (this.near(Vector._zVector)) return 5;
+    return null;
+  }
+
+}
+
+/**
+ * Defines a brush plane from 3 Vectors - points on the plane.
+ *
+ * Supports parsing VMF plane strings and outputting MAP file format
+ * plane strings when `toString` is invoked.
+ */
+class Plane {
+
+  constructor (p1, p2, p3) {
+    if (!(p1 instanceof Vector)) throw "Point 1 is not a Vector";
+    if (!(p2 instanceof Vector)) throw "Point 2 is not a Vector";
+    if (!(p3 instanceof Vector)) throw "Point 3 is not a Vector";
+    this.points = [p1, p2, p3];
+  }
+
+  static fromString (str) {
+    return new Plane(...str.replaceAll(")", "").split("(").slice(1).map(Vector.fromString));
+  }
   toString () {
-    return `${this.x} ${this.y} ${this.z}`;
+    return `( ${this.points[0]} ) ( ${this.points[1]} ) ( ${this.points[2]} )`;
+  }
+
+  getNormal () {
+    const v1 = this.points[1].copy().sub(this.points[0]);
+    const v2 = this.points[2].copy().sub(this.points[0]);
+    return v2.cross(v1).normalize();
+  }
+
+}
+
+/**
+ * Given a Portal 2 material, returns the corresponding ND texture.
+ *
+ * @param {string} material Portal 2 material path
+ * @returns {string} Narbacular Drop texture
+ */
+function convertMaterial (material) {
+  material = material.toLowerCase().replace("\\", "/");
+  if (material.startsWith("tile/white_floor_tile")) return "ROCK_FLOOR1";
+  if (material.startsWith("tile/white_wall_tile")) return "ROCK_WALL1";
+  if (material.startsWith("metal/black_floor_metal")) return "METAL_PANEL2";
+  if (material.startsWith("metal/black_wall_metal")) return "METAL_PANEL1";
+  if (material === "anim_wp/framework/backpanels_cheap") return "METAL_PANEL3";
+  if (material === "anim_wp/framework/squarebeams") return "METAL_PANEL3";
+  if (material === "glass/glasswindow007a_less_shiny") return "CHAINLINK";
+  if (material === "metal/metalgrate018") return "CHAINLINK";
+  if (material.startsWith("tools/")) return "AAATRIGGER";
+  /**
+   * For unrecognized textures, we return AAATRIGGER on PTI maps, but
+   * Hammer maps likely do actually have a solid there, so we return
+   * CHAINLINK to indicate an obvious placeholder.
+   */
+  if (isPTI) return "AAATRIGGER";
+  return "CHAINLINK";
+}
+
+/**
+ * Defines a brush side with `toString`/`fromString` methods for easy
+ * conversion from VMF to MAP format.
+ */
+class Side {
+
+  constructor (plane, uparams, vparams, rotation, material) {
+    this.plane = plane;
+    this.uparams = uparams;
+    this.vparams = vparams;
+    this.rotation = rotation;
+    this.material = material;
+  }
+
+  static fromString (plane, uaxis, vaxis, rotation, material) {
+    return new Side(
+      Plane.fromString(plane),
+      uaxis.replaceAll("[", "").replaceAll("]", "").split(" ").map(Number),
+      vaxis.replaceAll("[", "").replaceAll("]", "").split(" ").map(Number),
+      rotation || 0,
+      material || "AAATRIGGER"
+    );
+  }
+  static fromVMF (side) {
+    return Side.fromString(side.plane, side.uaxis, side.vaxis, side.rotation, convertMaterial(side.material));
+  }
+  toString () {
+    return (
+      this.plane.toString() + " " +
+      this.material + " " +
+      `[ ${this.uparams.slice(0, 4).join(" ")} ] ` +
+      `[ ${this.vparams.slice(0, 4).join(" ")} ] ` +
+      this.rotation + " " +
+      this.uparams[4] + " " +
+      // The trailing whitespace here is semantically significant
+      this.vparams[4] + " "
+    );
+  }
+
+  scale (factor) {
+    for (let i = 0; i < this.plane.points.length; i ++) {
+      this.plane.points[i].scale(factor);
+    }
+    return this;
   }
 
 }
@@ -266,12 +440,8 @@ function parseHammerEntity (entity) {
   const origin = Vector.fromString(entity.origin);
   origin.scale(unitScale);
 
-  if (entity.targetname === "@entry_door") {
-    /**
-     * The entry door is the spawnpoint, to avoid elevators.
-     * This should likely be a looser check? Decompiled maps won't contain
-     * the original instance's name verbatim.
-     */
+  if (entity.targetname === "@entry_door" || entity.targetname === "door_0-testchamber_door") {
+    // The entry door is the spawnpoint, to avoid elevators.
     createEntity({
       classname: "player_respawn",
       origin
@@ -285,11 +455,12 @@ function parseHammerEntity (entity) {
       target: "exit_counter"
     });
     buttonCount ++;
-  } else if (entity.targetname === "@exit_door") {
+  } else if (entity.targetname === "@exit_door" || entity.targetname === "@exit_door-testchamber_door") {
     /**
-     * The exit door maps to `level_end`. See `parseEditorEntity` for
-     * notes about orientation.
+     * The exit door maps to `level_end`. Since the exit cannot be rotated,
+     * this is temporarily disabled to prevent severe glitches.
      */
+    return;
     createEntity({
       classname: "level_end",
       origin: origin.add(new Vector(0, 96 * unitScale, 128)),
@@ -307,6 +478,52 @@ function parseHammerEntity (entity) {
       origin,
       weight: 150,
       scale: 2.4
+    });
+  } else if (entity.classname === "light") {
+    /**
+     * Basic lights are mapped to `light_point`.
+     * Colors are inherited directly. Range is hardcoded, because
+     * falloff distances are hardly ever specified.
+     */
+    const _light = entity._light.split(" ").map(Number);
+    const brightness = _light[3] / 255;
+    createEntity({
+      classname: "light_point",
+      origin,
+      _r: Math.floor(_light[0] * brightness),
+      _g: Math.floor(_light[1] * brightness),
+      _b: Math.floor(_light[2] * brightness),
+      _range: 300
+    });
+  } else if (entity.classname === "light_spot") {
+    /**
+     * Nearly direct mapping of `light_spot`, as similar entities exist in
+     * both games. Angles are converted to a forward vector.
+     */
+    const _light = entity._light.split(" ").map(Number);
+    const brightness = _light[3] / 255;
+    const fvec = Vector.fromAngles(Vector.fromString(entity.angles));
+    createEntity({
+      classname: "light_point",
+      origin,
+      _cone1: entity._inner_cone,
+      _cone2: entity._cone,
+      _r: Math.floor(_light[0] * brightness),
+      _g: Math.floor(_light[1] * brightness),
+      _b: Math.floor(_light[2] * brightness),
+      _range: 500,
+      _x: fvec.x,
+      _y: fvec.y,
+      _z: fvec.z
+    });
+  } else if (false) {
+    // See `parseEditorEntity` for notes about faith plate implementation.
+    origin.add(new Vector(0, 0, -64 * unitScale));
+    createEntity({
+      classname: "boulder",
+      origin,
+      speed: -1,
+      axis_choice: 5
     });
   }
 
@@ -342,86 +559,84 @@ createEntity({
   threshold: buttonCount
 });
 
-/**
- * Given a Portal 2 material, returns the corresponding ND texture.
- *
- * @param {string} material Portal 2 material path
- * @returns {string} Narbacular Drop texture
- */
-function convertMaterial (material) {
-  material = material.toLowerCase().replace("\\", "/");
-  if (material.startsWith("tile/white_floor_tile")) return "ROCK_FLOOR1";
-  if (material.startsWith("tile/white_wall_tile")) return "ROCK_WALL1";
-  if (material.startsWith("metal/black_floor_metal")) return "METAL_PANEL2";
-  if (material.startsWith("metal/black_wall_metal")) return "METAL_PANEL1";
-  if (material === "anim_wp/framework/backpanels_cheap") return "METAL_PANEL3";
-  if (material === "anim_wp/framework/squarebeams") return "METAL_PANEL3";
-  if (material === "glass/glasswindow007a_less_shiny") return "CHAINLINK";
-  if (material === "metal/metalgrate018") return "CHAINLINK";
-  if (material.startsWith("tools/")) return "AAATRIGGER";
-  /**
-   * For unrecognized textures, we return AAATRIGGER on PTI maps, but
-   * Hammer maps likely do actually have a solid there, so we return
-   * CHAINLINK to indicate an obvious placeholder.
-   */
-  if (isPTI) return "AAATRIGGER";
-  return "CHAINLINK";
-}
-
 // Iterates through all solids and adds them to the output map string
 for (const solid of json.world.solid) {
+  // Solids without sides aren't possible in Narbacular Drop
   if (!solid.side) continue;
 
   /**
-   * For now, we're treating ALL solids as `collidable_geometry`. This is
-   * not only unoptimal, but also causes some weird bugs, and doesn't
-   * allow for side-specific portalability. It's by far the simplest
-   * approach, but ideally this would be replaced with literally anything
-   * else. Even 6 `func_wall`s would do better in most cases.
+   * Whether the solid is portalable and/or seethrough.
+   * Used only for `collidable_geometry`.
    */
-  output += `{\n"classname" "collidable_geometry"\n`;
-
-  // Whether the solid is portalable and/or seethrough
   let portalable = false, seethrough = false;
-  // String containing constructed definitions for all sides of this solid
-  let sidesOutput = "";
+  /**
+   * Whether this solid is an axis-aligned wall. We start by assuming that
+   * it is, and then aim to disprove that as we iterate over all sides.
+   * The first disproving factor is if the amount of sides is not exactly 6.
+   */
+  let isWall = solid.side.length === 6;
 
-  // Iterate through each side, converting it from VMF format to MAP
-  for (const side of solid.side) {
+  // Array of Side objects, representing the sides of this solid
+  const solidSides = solid.side.map(s => Side.fromVMF(s).scale(unitScale));
+  // Array of axis indices for each of the sides
+  const axes = [];
 
-    // The plane could be copied verbatim, but we have to scale it first
-    // csg.exe also applies semantic significance to whitespace, we adjust for that
-    let plane = "";
-    const planePoints = side.plane.split("(").slice(1).map(c => c.split(")")[0].trim());
-    for (let i = 0; i < 3; i ++) {
-      plane += `( ${planePoints[i].split(" ").map(c => parseFloat(c) * unitScale).join(" ")} )`;
-      if (i !== 2) plane += " ";
+  for (const side of solidSides) {
+
+    // Determine the facing axis of this side from its normal vector
+    const axis = side.plane.getNormal().getAxis();
+    axes.push(axis);
+    // If this side isn't axis aligned, the solid can't be a wall
+    if (axis === null) isWall = false;
+
+    // If at least one of the materials is rock, a collidable_geometry
+    // created from this brush would have to be portalable.
+    if (side.material.startsWith("ROCK_")) portalable = true;
+    // If at least one of the materials is chainlink, this is a seethrough
+    // collidable_geometry and therefore also cannot be a wall.
+    else if (side.material === "CHAINLINK") {
+      seethrough = true;
+      isWall = false;
     }
-
-    // Reorder the u/v coordinates and scale parameters
-    // We're also adjusting for the previously mentioned semantic whitespace
-    const upos = side.uaxis.slice(1).split("]")[0].replaceAll("  ", " ");
-    const vpos = side.vaxis.slice(1).split("]")[0].replaceAll("  ", " ");
-    // Textures are scaled by unitScale, too
-    const uscale = parseFloat(side.uaxis.split("] ")[1].trim()) * unitScale;
-    const vscale = parseFloat(side.vaxis.split("] ")[1].trim()) * unitScale;
-
-    // Convert to Narbacular Drop texture, assign properties based on it
-    const texture = convertMaterial(side.material);
-    if (texture.startsWith("ROCK_")) portalable = true;
-    if (texture === "CHAINLINK") seethrough = true;
-
-    // Add constructed string to string of all sides
-    sidesOutput += `${plane} ${texture} [ ${upos} ] [ ${vpos} ] ${side.rotation} ${uscale.toFixed(3)} ${vscale.toFixed(3)} \n`;
 
   }
 
-  // Complete the solid definition - set portalability and transparency
-  if (!portalable) output += `"sfx_type" "1"\n`;
-  if (seethrough) output += `"spawnflags" "1"\n`;
-  output += `{\n`;
-  output += sidesOutput;
-  output += `}\n}\n`;
+  if (isWall) {
+    /**
+     * For axis-aligned walls, we create up to 6 brush entities
+     * representing each side of the wall. This allows for per-face
+     * portalability, and prevents some glitches with collidable_geometry.
+     */
+    for (let i = 0; i < solidSides.length; i ++) {
+      const side = solidSides[i];
+      const axis = axes[i];
+
+      // Skip faces that aren't going to be rendered anyway
+      if (side.material === "AAATRIGGER") continue;
+
+      // Define the brush entity and add it to the output map file string
+      output += `{\n"classname" "func_wall"\n`;
+      output += `"axis_choice" "${axis}"\n`;
+      output += `"wall_type" "${side.material.startsWith("ROCK_") ? 0 : 1}"\n`;
+      output += "{\n";
+      output += solidSides.join("\n");
+      output += "\n}\n}\n";
+
+    }
+  } else {
+    /**
+     * Complex geometries are ironically much simpler - we simply dump all
+     * of the sides into a single `collidable_geometry` entity and set
+     * the properties accordingly.
+     * The tradeoff is that these are less flexible.
+     */
+    output += `{\n"classname" "collidable_geometry"\n`;
+    if (!portalable) output += `"sfx_type" "1"\n`;
+    if (seethrough) output += `"spawnflags" "1"\n`;
+    output += `{\n`;
+    output += solidSides.join("\n");
+    output += `\n}\n}\n`;
+  }
 
 }
 
